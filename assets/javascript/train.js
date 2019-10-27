722fc4da4ce87ba0688912411ad643e00484c227
Khadijah Grimes <khadijahgrimes7@gmail.com>
2019-10-26 19:40:05 -0500

    updates

---
+++ assets/javascript/train.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAD-vhtlIQVBl9cLIiczvmqx7nZKBRBOYU",
+    authDomain: "train-scheduler-78757.firebaseapp.com",
+    databaseURL: "https://train-scheduler-78757.firebaseio.com",
+    projectId: "train-scheduler-78757",
+    storageBucket: "train-scheduler-78757.appspot.com",
+    messagingSenderId: "248995229788",
+    appId: "1:248995229788:web:97c0d382fe69437518f699",
+    measurementId: "G-DH0S2T403B"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
